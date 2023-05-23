568b1fd7acce62ecd784c795560f1ab0f5669455
Phil T <philongthan@gmail.com>
2023-05-23 10:06:54 -0600

    Merge origin/navbar into feat/home-company-login-screens

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SelectCompanyComponent } from './select-company/select-company.component';
+import { AnnouncementsComponent } from './announcements/announcements.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'select-company', component: SelectCompanyComponent },
+  { path: 'announcements', component: AnnouncementsComponent },
 ];
 
 @NgModule({

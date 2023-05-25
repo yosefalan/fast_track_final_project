711dd2050e21bb1a1fa642391ece3bbf75b7ed48
Warren Wang <wang.warren123@gmail.com>
2023-05-25 04:10:00 -0500

    cleaned up merge, implemented # of projects, added modal

--- frontend/src/app/teams/teams.component.ts
+++ frontend/src/app/teams/teams.component.ts
@@ -7,6 +7,10 @@ import Team from '../models/Team';
 import { CompanyService } from '../services/company.service';
 import { HttpClient } from '@angular/common/http';
 import { ProjectsComponent } from '../projects/projects.component';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import BasicUser from '../models/BasicUser';
+import { show } from '../overlay/helper';
+
 
 @Component({
   selector: 'app-teams',
@@ -15,13 +19,30 @@ import { ProjectsComponent } from '../projects/projects.component';
 })
 export class TeamsComponent {
   user: User | null = null;
-  // company: Company | null = null;
-  teamCards: any[] = [];
-  projects: any[] = [];
+  company: Company | null = null;
   teamId: number | null = null;
-  companyId: number | null = null;
-  teams: any[] | undefined;
   userId: number | null = null;
+  companyId: number | null = null;
+  teams: any[] = [];
+  projectCountsPerTeam: { [teamId: number]: number } = {};
+  
+
+  availableMembers: BasicUser[] = [];
+  selectedMembers: BasicUser[] = [];
+
+  newTeamForm: FormGroup = new FormGroup({
+    name: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(1),
+    ]),
+    description: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(1),
+    ]),
+    members: new FormControl<string[]>([]),
+  });
+
+ 
 
   constructor(
     private router: Router,
@@ -31,24 +52,38 @@ export class TeamsComponent {
   ) {}
 
   ngOnInit(): void {
+    this.companyData.selectedCompany.subscribe((company) => {
+      this.company = company;
+      if (company) {
+        this.companyId = company.id;
+      }
+    });
+
     this.userData.loggedInUser.subscribe((user) => {
       this.user = user;
-      this.teams = this.user?.teams;
-      this.companyId = this.user?.companies[0].id ?? null;
-      this.userId = user?.id ?? null;
+      if (user === null) {
+        this.router.navigateByUrl('/');
+      }
+      else {
+        this.userId = user.id;
+        if (user.admin === false) {
+          this.fetchWorkerTeams();
+        }
+        else if (user.admin === true) {
+          this.fetchAdminTeams();
+        }
+      }
     });
-    this.fetchTeams();
-    this.fetchTeamsAdmin();
   }
 
-  fetchTeamsAdmin(): void {
-    const companyId = this.companyId;
-    const url = `http://localhost:8080/company/${companyId}/teams`;
+  fetchAdminTeams(): void {
+    const url = `http://localhost:8080/company/${this.companyId}/teams`;
 
     this.http.get<any[]>(url).subscribe(
       (response) => {
         this.teams = response;
-        console.log('*******', response);
+        this.fetchProjectCounts();
+        this.fetchAvailableMembers();
       },
       (error) => {
         console.error('Error fetching teams:', error);
@@ -56,21 +91,99 @@ export class TeamsComponent {
     );
   }
 
-  fetchTeams(): void {
-    console.log(this.user?.id);
-    const userId = this.userId;
-    console.log(typeof userId);
-
-    const url = `http://localhost:8080/users/${userId}/teams`;
+  fetchWorkerTeams(): void {
+    const url = `http://localhost:8080/users/${this.userId}/teams`;
 
     this.http.get<any[]>(url).subscribe(
       (response) => {
         this.teams = response;
-        console.log(response);
+        this.fetchProjectCounts();
       },
       (error) => {
         console.error('Error fetching teams:', error);
       }
     );
   }
+
+  fetchProjectCounts(): void {
+    for (let team of this.teams) {
+      const url = `http://localhost:8080/company/${this.companyId}/teams/${team.id}/projects`;
+
+      this.http.get<any[]>(url).subscribe(
+        (response) => {
+          this.projectCountsPerTeam[team.id] = response.length;
+        },
+        (error) => {
+          console.error('Error fetching projects:', error);
+        }
+      );
+    }
+  }
+
+  fetchAvailableMembers(): void {
+    const url = `http://localhost:8080/company/${this.companyId}/users`;
+
+    this.http.get<any[]>(url).subscribe(
+      (response) => {
+        this.availableMembers = response;
+      },
+      (error) => {
+        console.error('Error fetching teams:', error);
+      }
+    );
+  }
+
+  updateSelectedMembers(): void {
+    const selectedMemberIds = this.newTeamForm.value.members;
+    this.selectedMembers = this.availableMembers.filter(
+      (member) => selectedMemberIds.includes(member.id)
+    );
+  }
+
+  removeSelectedMember(memberId: number): void {
+    const selectedMemberIds = this.newTeamForm.value.members;
+    const index = selectedMemberIds.indexOf(memberId);
+    if (index !== -1) {
+      selectedMemberIds.splice(index, 1);
+      this.selectedMembers = this.selectedMembers.filter(
+        (member) => member.id !== memberId
+      );
+    }
+  }
+
+  showNewTeamModal(): void {
+    console.log("show new team form modal");
+    show();
+  }
+
+  onNewTeamFormSubmit(): void {
+    if (this.companyId) {
+      fetch(`http://localhost:8080/company/${this.companyId}/teams`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: this.newTeamForm.get('name')?.value,
+        description: this.newTeamForm.get('description')?.value,
+        teammateIds: this.newTeamForm.get('members')?.value,
+      }),
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(`Status: ${res.status}`);
+          }
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error('Error creating team:', error);
+        });
+    }
+  }
+
+
 }
